c7b7de84cdb27453cca00ee39b135950e89ed891
maricel <maricelmb@gmail.com>
2022-08-12 09:14:09 +0800

    S3L63_Setting up Axios

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+
 import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,13 +14,20 @@ function App() {
   >(undefined);
   const [editMode, setEditMode] = useState(false);
 
+  // useEffect(() => {
+  //   axios
+  //     .get<Activity[]>("http://localhost:5000/api/activities")
+  //     .then((response) => {
+  //       console.log(response);
+  //       setActivities(response.data);
+  //     });
+  // }, []);
+
   useEffect(() => {
-    axios
-      .get<Activity[]>("http://localhost:5000/api/activities")
-      .then((response) => {
-        console.log(response);
-        setActivities(response.data);
-      });
+    agent.Activities.list().then((response) => {
+      console.log(response);
+      setActivities(response);
+    });
   }, []);
 
   function handleSelectActivity(id: string) {
@@ -45,13 +53,13 @@ function App() {
           ...activities.filter((x) => x.id !== activity.id),
           activity,
         ])
-      : setActivities([...activities, {...activity, id: uuid()}]); // create
+      : setActivities([...activities, { ...activity, id: uuid() }]); // create
     setEditMode(false);
     setSelectedActivity(activity);
   }
 
-  function handleDeleteActivity(id: string){
-    setActivities([...activities.filter(x => x.id  !== id)])
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter((x) => x.id !== id)]);
   }
 
   return (
